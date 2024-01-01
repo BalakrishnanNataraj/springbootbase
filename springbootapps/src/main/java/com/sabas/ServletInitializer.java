1211642d19284300803db1ef67471bc689b563be
balakrishnannataraj <balakrishnan.swd@gmail.com>
2024-01-01 22:23:43 +0530

    profile created

---
+++ springbootapps/src/main/java/com/sabas/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sabas;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootappsApplication.class);
+	}
+
+}
---
+++ springbootapps/src/main/java/com/sabas/SpringbootappsApplication.java
@@ -0,0 +1,13 @@
+package com.sabas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootappsApplication.class, args);
+	}
+
+}
